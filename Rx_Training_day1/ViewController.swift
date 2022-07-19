1a78bcd4c0761e29f4a5b981133841789557d572
anies1212 <all.for.me1212sna@gmail.com>
2022-07-19 22:00:16 +0900

    Initial Commit

---
+++ Rx_Training_day1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rx_Training_day1
+//
+//  Created by anies1212 on 2022/07/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
